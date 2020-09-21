d06b837d9442eefdba551529069f2a8c107b969c
Labirintami <tatyana.pashkovich@gmail.com>
2020-09-21 16:17:50 +0300

    [update]  migrating docs from old version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,104 +1,175 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+    title: 'DHTMLX RichText Documentation',
+    tagline: 'DHTMLX RichText Documentation',
+    url: 'https://docs.webix.com/richtext/',
+    baseUrl: '/richtext/',
+    onBrokenLinks: 'ignore',
+    favicon: 'img/favicon.ico',
+    organizationName: 'labirintami', // Usually your GitHub org/user name.
+    projectName: 'doc', // Usually your repo name.
+    themeConfig: {
+        colorMode: {
+            // "light" | "dark"
+            defaultMode: 'light',
+
+            // Hides the switch in the navbar
+            // Useful if you want to support a single color mode
+            disableSwitch: false,
+
+            // Should we use the prefers-color-scheme media-query,
+            // using user system preferences, instead of the hardcoded defaultMode
+            respectPrefersColorScheme: true,
+
+            // Dark/light switch icon options
+            switchConfig: {
+                darkIcon: '🌙',
+                darkIconStyle: {
+                    marginLeft: '0px',
+                },
+                lightIcon: '☀️',
+                lightIconStyle: {
+                    marginLeft: '0px',
+                },
+            },
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        algolia: {
+            // This is a read-only, search-only key served directly by the front-end, managed by Algolia via their
+            // free DocSearch program. The key is not sensitive. See https://docsearch.algolia.com/ for more details.
+            apiKey: '421aa9b07cc441097c4db7f264b63a6b',
+            indexName: 'richtext',
+
+            // Optional: see doc section bellow
+            contextualSearch: true,
+
+            // Optional: Algolia search parameters
+            searchParameters: {},
+
+            //... other Algolia params
         },
-      ],
+        prism: {
+            theme: require('prism-react-renderer/themes/github'),
+            darkTheme: require('prism-react-renderer/themes/dracula'),
+        },
+        navbar: {
+            title: 'RichText Documenation',
+            logo: {
+                alt: 'DHTMLX RichText Documenation',
+                src: 'img/dhtmlx_logo.svg',
+                href: 'https://docs.dhtmlx.com/',
+
+            },
+            items: [{
+                    label: 'Examples',
+                    href: 'https://docs.dhtmlx.com/richtext/samples/',
+                    position: 'right',
+                },
+                {
+                    label: 'Forum',
+                    href: 'https://forum.dhtmlx.com/c/widgets/richtext',
+                    position: 'right'
+                },
+                {
+                    label: 'Support',
+                    href: 'https://dhtmlx.com/docs/technical-support.shtml',
+                    position: 'right',
+                },
+                {
+                    label: 'Download',
+                    href: 'https://dhtmlx.com/docs/products/dhtmlxRichText/download.shtml',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [{
+                    title: 'Development center',
+                    items: [{
+                            label: 'Download RichText',
+                            href: 'https://dhtmlx.com/docs/products/dhtmlxRichText/download.shtml',
+                            position: 'right',
+                        },
+                        {
+                            label: 'Examples',
+                            href: 'https://docs.dhtmlx.com/richtext/samples/',
+                            position: 'right',
+                        },
+                        {
+                            label: 'Blog',
+                            href: 'https://dhtmlx.com/blog/tag/richtext/',
+                        },
+                        {
+                            label: 'Forum',
+                            href: 'https://forum.dhtmlx.com/c/widgets/richtext',
+                            position: 'right'
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [{
+                            label: 'GitHub',
+                            href: 'https://github.com/DHTMLX',
+                        },
+
+                        {
+                            label: 'Youtube',
+                            href: 'https://www.youtube.com/user/dhtmlx',
+                        },
+                        {
+                            label: 'Facebook',
+                            href: 'https://www.facebook.com/dhtmlx',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/dhtmlx',
+                        },
+                        {
+                            label: 'Linkedin',
+                            href: 'https://www.linkedin.com/groups/3345009/',
+                        },
+
+                    ],
+                },
+                {
+                    title: 'Company',
+                    items: [{
+                            label: 'About us',
+                            href: 'https://dhtmlx.com/docs/company.shtml',
+                        },
+                        {
+                            label: 'Contact us',
+                            href: 'https://dhtmlx.com/docs/contact.shtml',
+                        },
+                        {
+                            label: 'Licensing',
+                            href: 'https://dhtmlx.com/docs/products/dhtmlxRichText/#editions-licenses',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} DHTMLX, Inc. Built with Docusaurus.`,
+        },
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Style Guide',
-              to: 'docs/',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/Labirintami/doc/edit/master/',
+                    routeBasePath: '/',
+                },
+                // blog: {
+                //     showReadingTime: true,
+                //     // Please change this to your repo.
+                //     editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                // },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,45 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
-};
+    docs: [{
+            type: "doc",
+            id: "how_to_start",
+        },
+        {
+            type: "doc",
+            id: "overview",
+        },
+        {
+            type: "category",
+            label: "API",
+            collapsed: false,
+            items: [
+                "api/overview",
+                "api/methods",
+                "api/events_bus",
+                "api/events",
+                "api/properties",
+                "api/editor_api_methods",
+            ],
+        },
+        {
+            type: "doc",
+            id: "whats_new",
+        },
+        {
+            type: "category",
+            label: "Guides",
+            collapsed: false,
+            items: [
+                "guides/initialization",
+                "guides/configuration",
+                "guides/localization",
+                "guides/loading_data",
+                "guides/working_with_richtext",
+                "guides/customization",
+                "guides/event_handling",
+                "guides/angular_integration",
+                "guides/react_integration",
+                "guides/vuejs_integration",
+            ],
+        },
+    ],
+}
